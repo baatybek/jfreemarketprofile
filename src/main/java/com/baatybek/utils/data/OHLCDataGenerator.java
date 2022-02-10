13678814a1131774a68f59d4f7267a9e06a65116
baatybek <beksultanbaatyrbekov@gmail.com>
2022-02-10 14:53:13 +0100

    ohcl data generator from csv

---
+++ src/main/java/com/baatybek/utils/data/OHLCDataGenerator.java
@@ -0,0 +1,51 @@
+package com.baatybek.utils.data;
+
+import com.baatybek.utils.csv.model.CSVModel;
+import com.baatybek.utils.csv.reader.MCSVReader;
+import com.baatybek.utils.datetime.DateTimeUtility;
+import org.jfree.data.time.FixedMillisecond;
+import org.jfree.data.time.ohlc.OHLCItem;
+import org.jfree.data.time.ohlc.OHLCSeries;
+import org.jfree.data.time.ohlc.OHLCSeriesCollection;
+import org.jfree.data.xy.OHLCDataset;
+
+import java.io.FileReader;
+import java.util.List;
+
+public final class OHLCDataGenerator {
+
+    private static String datasetPath = "/home/baatybek/Workspaces/Thesis/jfreemarketprofile/src/main/resources/csv/ohlc-dataset.csv";
+
+    public static OHLCDataset generate() throws Exception {
+        List<CSVModel> csvData = readDataFromCSV();
+        OHLCSeries series = new OHLCSeries("08/07/2021");
+
+        for(CSVModel model : csvData) {
+            OHLCItem item = convertCSVModelToOHLCItem(model);
+            series.add(item);
+        }
+
+        OHLCSeriesCollection seriesCollection = new OHLCSeriesCollection();
+        seriesCollection.addSeries(series);
+        return seriesCollection;
+    }
+
+    public static void setDatasetPath(String datasetPath) {
+        OHLCDataGenerator.datasetPath = datasetPath;
+    }
+
+    private static List<CSVModel> readDataFromCSV() throws Exception {
+        FileReader fileReader = new FileReader(datasetPath);
+        return MCSVReader.readAll(fileReader);
+    }
+
+    private static OHLCItem convertCSVModelToOHLCItem(CSVModel model) throws Exception {
+        FixedMillisecond time = DateTimeUtility.generateFixMilliSec(model.getDateTime());
+        double open = Double.parseDouble(model.getOpen());
+        double high = Double.parseDouble(model.getHigh());
+        double low = Double.parseDouble(model.getLow());
+        double close = Double.parseDouble(model.getClose());
+
+        return new OHLCItem(time, open, high, low, close);
+    }
+}
---
+++ src/test/java/com/baatybek/properties/ConstProperties.java
@@ -0,0 +1,5 @@
+package com.baatybek.properties;
+
+public final class ConstProperties {
+    public static final String DATASET_PATH = "/home/baatybek/Workspaces/Thesis/jfreemarketprofile/src/test/resources/csv/readertest.csv";
+}
